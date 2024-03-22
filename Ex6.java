f86392a7a3170b2e5c35a33de5141b86e5ffaad4
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:27:11 -0300

    Exercise 6

--- Ex6.java
+++ Ex6.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
-public class Ex6 {
+
+public class Ex6{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Please input a Celsius temperature: ");
-        double C = input.nextDouble();
-        input.nextLine();
-        double F = (9 * C + 160) / 5;
-        System.out.printf("-------------------------------------\n");
-        System.out.printf("%.2f Fahrenheit\n", F);
+            Scanner input = new Scanner(System.in);
+            
+            double nota[] = new double[5];
+            double pesos[] = new double[5];
+            double weights = 0;
+            double weightPlusNotas = 0;
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("Questão número " + (i+1) + "\n");
+                System.out.printf("Por favor insira a nota do aluno nessa questão:\n");
+                nota[i] = input.nextDouble();
+                System.out.printf("Por favor insira o peso da questão:\n");
+                pesos[i] = input.nextDouble();
+                input.nextLine();
+                weights += pesos[i];
+                weightPlusNotas += pesos[i] * nota[i];
+            }
+            System.out.println("A media ponderada do aluno é " + (weightPlusNotas/weights));
+
     }
 }
+

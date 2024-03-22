c044c443c1c02ef2adc14101906fbbfca1f0b96e
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:28:33 -0300

    Exercise 11

--- Ex11.java
+++ Ex11.java
@@ -1,34 +1,25 @@
 import java.util.Scanner;
-
-public class Ex11 {
+public class Ex11{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        int n[] = new int[3];
-        for (int i = 0; i < 3; i++) {
-            System.out.printf("Insira o " + (i+1) + "° valor: ");
-            n[i] = input.nextInt();
-        }
-        input.nextLine();
-        System.out.printf("n[3] = ");
-        for (int i = 0; i < n.length; i++) {
-            System.out.printf(n[i] + " ");
-        }
-        System.out.println();
-        //Para solucionar este exercício nós utilizamos a Ordenação por trocas
-        for (int i = 0; i < n.length - 1/*3 - 1 = 2. 0 1 2*/; i++) {//Preparando o passo a passo
-            for (int j = 0; j < n.length - i - 1/*3 - i - 1 = 0 or 1 or 2 */ ; j++) {//Preparando o valor para comparação
-                if (n[j] > n[j + 1]){
-                    int value = n[j];
-                    n[j] = n[j+1];
-                    n[j+1] = value;
-                }   
+            Scanner input = new Scanner(System.in);
+            
+            double A[] = new double[5];
+            double B[] = new double[5];
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("Por favor insira valor do A [%d](Integer):\n", i);
+                A[i] = input.nextDouble();
+                System.out.printf("Por favor insira valor do B [%d](Integer):\n", i);
+                B[i] = input.nextDouble();
+                input.nextLine();
             }
-        }
-        //We could just use arrays.sort() but Bubble sort is more fun :)
-        System.out.printf("n[3] = ");
-        for (int i = 0; i < n.length; i++) {
-            System.out.printf(n[i] + " ");
-        }
-        System.out.println();
+            double C[] = new double[5];
+            
+            System.out.println("C is equals to: ");
+            for (int i = 0; i < C.length; i++) {
+                    C[i] = B[i] * A[i];
+                    System.out.printf(" %.2f", C[i]);
+            }
+            System.out.printf("\n");
+
     }
 }

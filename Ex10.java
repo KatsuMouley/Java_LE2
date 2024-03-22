39de9849b5a9d3445555b6f40382ac2907c7b494
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:28:18 -0300

    Exercise 10

--- Ex10.java
+++ Ex10.java
@@ -1,30 +1,23 @@
 import java.util.Scanner;
-public class Ex10 {
+public class Ex10{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Please insert a value between 1 and 5: ");
-        int n = input.nextInt();
-        input.nextLine();
-        switch (n) {
-            case 1:
-                System.out.println("Este é o número Um");
-                break;
-            case 2:
-                System.out.println("Este é o número Dois");
-                break;
-            case 3:
-                System.out.println("Este é o número Três");
-                break;
-            case 4:
-                System.out.println("Este é o número Quatro");
-                break;
-            case 5:
-                System.out.println("Este é o número Cinco");
-                break;
-        
-            default:
-                System.out.println("Número invalido!");
-                break;
-        }   
+            Scanner input = new Scanner(System.in);
+            
+            int A[] = new int[5];
+            int B[] = new int[5];
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("Por favor insira valor do A [%d](Integer):\n", i);
+                A[i] = input.nextInt();
+                input.nextLine();
+            }
+            for (int i = 0; i < A.length; i++) {
+                System.out.printf(" %d", A[i]);
+            }
+            System.out.printf("\n");
+            for (int i = 4; i >= 0; i--) {
+                B[i] = A[i];
+                System.out.printf(" %d", B[i]);
+            }
+            System.out.printf("\n");
     }
 }

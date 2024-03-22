0261511b5e51d7be3345d892ab20e0803f032ce8
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:29:54 -0300

    Exercise 15

--- Ex15.java
+++ Ex15.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
-public class Ex15 {
+public class Ex15{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("P1(x1,y1): ");
-        System.out.printf("Please input x1: ");
-        double x1 = input.nextDouble();
-        System.out.printf("Please input y1: ");
-        double y1 = input.nextDouble();
-
-        System.out.println("P2(x2,y2): ");
-        System.out.printf("Please input x2: ");
-        double x2 = input.nextDouble();
-        System.out.printf("Please input y2: ");
-        double y2 = input.nextDouble();
-        input.nextLine();
-        double d = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
-        System.out.printf("A distância entre ambos os pontos é de %.2f\n", d);
-                  
+            Scanner input = new Scanner(System.in);
+            double value[] = new double[5];
+            double n = 0;
+            for (int i = 0; i < value.length; i++) {    
+                do {
+                    System.out.printf("Por favor insira valor[%d] :\n", i+1);
+                    value[i] = input.nextDouble();
+                    if (value[i] < n ) {
+                        System.out.println("Por favor insira um valor que seja maior do que o valor anterior");
+                    } else {
+                    n = value[i];
+                    }
+                } while (value[i] < n);   
+            }
+            for (int i = 0; i < value.length; i++) {
+                System.out.printf("%.1f ", value[i]);
+            }
+            System.out.printf("\n");
     }
 }
+

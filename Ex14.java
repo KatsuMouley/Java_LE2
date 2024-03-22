168dd3f32bc55b4318d1b904b255b6de2ae608dc
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:29:38 -0300

    Exercise 14

--- Ex14.java
+++ Ex14.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
-
-public class Ex14 {
+public class Ex14{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Please input a1: ");
-        double a1 = input.nextDouble();
-        System.out.printf("Please input n: ");
-        double n = input.nextDouble();
-        System.out.printf("Please input r: ");
-        double r = input.nextDouble();
-        input.nextLine();
-        double an = a1 + (n-1) * r;
-        System.out.println("an = " + an);
-                  
+            Scanner input = new Scanner(System.in);
+            String arr1 = "";
+            String arr2 = "";
+            double value[] = new double[5];
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("Por favor insira um valor[%d]:\n", i+1);
+                value[i] = input.nextDouble();
+                arr1 += " " + value[i];
+                input.nextLine();
+            }
+            for (int i = 0; i < value.length; i++) {
+                System.out.printf("%.1f ", value[i]);   
+            }
+            double max = value[0];
+            int Indice = 0;
+            for (int i = 0; i < 5; i++) {
+                if (value[i] > max) {
+                    Indice = i;
+                    max = value[i];
+                }
+            }
+
+            double temp = value[value.length - 1];
+            value[value.length - 1] = max;
+            value[Indice] = temp;
+
+            System.out.printf("\n");
+            for (int i = 0; i < value.length; i++) {
+                System.out.printf("%.1f ", value[i]);   
+            }
+            System.out.printf("\n");
+
     }
 }

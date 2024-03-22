bc7da341af9313004d92e674e7c46983cf482bb1
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:28:06 -0300

    Exercise 9

--- Ex9.java
+++ Ex9.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
-
-public class Ex9 {
+public class Ex9{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Please input the A value(Integer): ");
-        int B = input.nextInt();
-        System.out.printf("Please input the B value(Integer): ");
-        int A = input.nextInt();
-        input.nextLine();
-        if (A == B) {
-            System.out.println("Both values "+ A +" and "+ B +" are equals.");
-        } else {
-            System.out.println("Both values "+ A +" and "+ B +" are different. The biggest number between them is " + Math.max(A, B));
-        }     
+            Scanner input = new Scanner(System.in);
+            
+            int A[] = new int[5];
+            int B[] = new int[5];
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("Por favor insira valor do A [%d](Integer):\n", i);
+                A[i] = input.nextInt();
+                System.out.printf("Por favor insira valor do B [%d](Integer):\n", i);
+                B[i] = input.nextInt();
+                input.nextLine();
+            }
+            int C[] = new int[5];
+            
+            System.out.println("C is equals to: ");
+            for (int i = 0; i < C.length; i++) {
+                if (i%2 == 0) {
+                    C[i] = A[i];
+                    System.out.println("Vetor de posição par " + C[i]);
+                } else {
+                    C[i] = B[i];
+                    System.out.println("Vetor de posição impar " + C[i]);
+                }
+            }
+
     }
 }

ac7735a686dde47453178165d606a82df26905fe
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:28:53 -0300

    Exercise 12

--- Ex12.java
+++ Ex12.java
@@ -1,51 +1,25 @@
 import java.util.Scanner;
+public class Ex12{
+    public static void run(){
+            Scanner input = new Scanner(System.in);
+            
+            double value[] = new double[12];
+            double product = 1;
+            for (int i = 0; i < 12; i++) {
+                System.out.printf("Por favor insira um valor[%d]:\n", i+1);
+                value[i] = input.nextDouble();
+                input.nextLine();
+            }
+            
+            for (int i = 0; i < 12; i++) {
+                if (value[i] > 0 && value[i] != 0 && value[i]%2 == 0) {
+                    product *= value[i];
+                    System.out.printf("%.2f * ",value[i]);
+                }
+            }
+            System.out.printf("1 \n");
+            System.out.printf("The final product is %.2f", product);
+            System.out.printf("\n");
 
-public class Ex12 {
-    public static void run() {
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Insira o número de um mês: ");
-        int n = input.nextInt();
-        input.nextLine();
-        switch (n) {
-            case 1:
-                System.out.println("Janeiro");
-                break;
-            case 2:
-                System.out.println("Fevereiro");
-                break;
-            case 3:
-                System.out.println("Março");
-                break;
-            case 4:
-                System.out.println("Abril");
-                break;
-            case 5:
-                System.out.println("Maio");
-                break;
-            case 6:
-                System.out.println("Junho");
-                break;
-            case 7:
-                System.out.println("Julho");
-                break;
-            case 8:
-                System.out.println("Agosto");
-                break;
-            case 9:
-                System.out.println("Setembro");
-                break;
-            case 10:
-                System.out.println("Outubro");
-                break;
-            case 11:
-                System.out.println("Novembro");
-                break;
-            case 12:
-                System.out.println("Dezembro");
-                break;
-            default:
-                System.out.println("Mês invalido!");
-                break;
-        }
     }
 }

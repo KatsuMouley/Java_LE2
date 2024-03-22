f154ec7df9e87b6986198aedb7a6a92255c23135
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-22 02:27:34 -0300

    Exercise 7

--- Ex7.java
+++ Ex7.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
-public class Ex7 {
+import java.util.Arrays;
+
+public class Ex7{
     public static void run(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Please input a number: ");
-        int n = input.nextInt();
-        input.nextLine();
-        if (n > 100 && n < 200) {
-            System.out.printf("The number %d is between 100 and 200\n", n);
-        } else {
-            System.out.printf("The number %d is NOT between 100 and 200\n", n);
-        }
+            Scanner input = new Scanner(System.in);
+            
+            double valor1[] = new double[5];
+            double valor2[] = new double[5];
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("Por favor insira valor do vetor1 [%d]:\n", i+1);
+                valor1[i] = input.nextDouble();
+                System.out.printf("Por favor insira valor do vetor2 [%d]:\n", i+1);
+                valor2[i] = input.nextDouble();
+                input.nextLine();
+            }
+            if (Arrays.equals(valor1, valor2)) {
+                System.out.println("Ambos arrays são iguais");
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("O vetor1["+(i+1)+"] É igual ao vetor2["+(i+1)+"] |"+ valor1[i] + " = " + valor2[i]);
+                }   
+            } else {
+                System.out.println("Ambos arrays NÃO são iguais");
+            }
+
     }
 }
+
